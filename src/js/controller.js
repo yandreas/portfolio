7f34d8736cf3d0b798509ead1961af42b1ae8a71
ymeier <yuye_andreas.meier@htwk-leipzig.de>
2024-03-21 01:18:29 +0100

    change form

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import sectionView from './views/sectionView';
+import contactView from './views/sectionView';
 
 init = function () {};
 init();
---
+++ src/js/views/contactView.js
@@ -0,0 +1,29 @@
+import View from './View.js';
+
+class contactView extends View {
+  #parentElement = document.querySelector('form');
+
+  constructor() {
+    super();
+    this.#addSubimtListener();
+  }
+
+  #addSubimtListener() {
+    const handleSubmit = e => {
+      e.preventDefault();
+      const myForm = document.getElementById('contact');
+      const formData = new FormData(myForm);
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData).toString(),
+      })
+        .then(() => console.log('Form successfully submitted'))
+        .catch(error => alert(error));
+    };
+
+    this.#parentElement.addEventListener('submit', handleSubmit);
+  }
+}
+
+export default new contactView();
